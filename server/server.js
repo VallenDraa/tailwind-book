aabfbb740ee13b55d5df7d79a21fef009fbd66e2
KoooYoung <Jestinevallendradwiputra@gmail.com>
2022-03-23 15:01:33 +0700

    added express server

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('listening on port 3000');
+});
